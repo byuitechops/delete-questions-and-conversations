f2c245ccfd10e1b6ce1d50e46095d85054b34432
Sam McGrath <mcg13004@byui.edu>
2017-12-12 17:31:18 -0700

    completed child module. ready for testing

--- main.js
+++ main.js
@@ -5,24 +5,42 @@
 /* Put dependencies here */
 
 /* Include this line only if you are going to use Canvas API */
-// const canvas = require('canvas-wrapper');
+const canvas = require('canvas-wrapper');
+const asyncLib = require('async');
 
 /* View available course object functions */
 // https://github.com/byuitechops/d2l-to-canvas-conversion-tool/blob/master/documentation/classFunctions.md
 
 module.exports = (course, stepCallback) => {
-    /* Create the module report so that we can access it later as needed.
-    This MUST be done at the beginning of each child module. */
-    course.addModuleReport('moduleName');
+    setTimeout(() => {
+        course.addModuleReport(`delete-questions-and-conversations`);
 
-    /* Used to log successful actions */
-    course.success('moduleName', 'moduleName successfully ...');
+        // Get the discussion boards
+        canvas.get(`/api/v1/courses/${course.info.canvasOU}/discussion_topics?search_term=Questions%20and%20Conversations`, (getErr, discussion_topics) => {
+            if (getErr) {
+                course.throwErr(`delete-questions-and-conversations`, getErr);
+                return;
+            }
+            course.success(`delete-questions-and-conversations`, `Retrieved ${discussion_topics.length} discussion topics`);
+            asyncLib.each(discussion_topics, (topic, callback) => {
+                canvas.delete(`/api/v1/courses/${course.info.canvasOU}/discussion_topics/${topic.id}`, (deleteErr, result) => {
+                    if (deleteErr) {
+                        callback(deleteErr);
+                        return;
+                    }
+                    course.success(`delete-questions-and-conversations`, `Deleted discussion topic: ${topic.title}`);
+                    callback(null);
+                });
+            }, (eachErr) => {
+                if (eachErr) {
+                    course.throwErr(`delete-questions-and-conversations`, eachErr);
+                    return;
+                }
+                course.success(`delete-questions-and-conversations`, `Finished deleting all discussion topics`);
+                stepCallback(null, course);
+            });
+        });
 
-    /* How to report an error (Replace "moduleName") */
-    // course.throwErr('moduleName', e);
+    }, 10000);
 
-    /* You should never call the stepCallback with an error. We want the
-    whole program to run when testing so we can catch all existing errors */
-
-    stepCallback(null, course);
 };
